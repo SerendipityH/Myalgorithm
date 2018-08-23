099ea10e759c76149d740a6736f13e8d9acb09d9
葛旗浩 <gqh@umshz.com>
2018-08-23 15:30:34 +0800

    commit

---
+++ src/basic/practice/practice1/Test2.java
@@ -0,0 +1,13 @@
+package practice1;
+//下列语句会打印出什么结果？给出解释。
+/*
+ *@author gqh
+ * @date 2018/08/23
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println('b');//b
+        System.out.println('b' + 'c');//196
+        System.out.println((char) ('a' + 4));//e  整数默认是int
+    }
+}
